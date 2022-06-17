8155fe075a841b5615eab1cb773b6d2a535a436f
Wicho <luiscalderonmiranda@gmail.com>
2022-06-16 22:50:12 -0500

    missing more styles on form

--- src/components/FormCard.jsx
+++ src/components/FormCard.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import '../styles/FormCard.css'
 
 function FormCard(){
-    return(
-        <form className='Form-container'>
-            <label> img</label>
-            <input type='file' name="" id="" />
-            <label>Title</label>
-            <input type='text' />
-            <label>Text</label>
-            <input type='text' />
-        </form>
-    );
+  return(
+    <form className='form-container'>
+      <label for='image_upload'>img</label>
+      <input 
+        type='file' 
+        name='image_upload'
+        id='image_upload'
+        accept='.jpg, .jpeg, .png' />
+      <label>Title</label>
+      <input type='text' />
+      <label>Text</label>
+      <input type='text' />
+      <button type="submit">Create</button>
+    </form>
+  );
 }
 
 export default FormCard;
