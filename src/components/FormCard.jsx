354a4fb0872a48447545b564da8db09c7da109d4
Wicho <luiscalderonmiranda@gmail.com>
2022-06-19 22:11:00 -0500

    image preview implemented

--- src/components/FormCard.jsx
+++ src/components/FormCard.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import '../styles/FormCard.css'
+import React, {useState} from 'react'
+import '../styles/FormCard.css';
 
 function FormCard(){
+
+  const [selectedImage, setSelectedImage] = useState();
+
+  const imageChange = (e) => {
+    if(e.target.files && e.target.files.length > 0){
+      setSelectedImage(e.target.files[0]);
+    }
+  }
+
   return(
     <form className='form-container'>
       <label htmlFor='image_upload' className='form-file-label'>img...</label>
@@ -10,7 +19,17 @@ function FormCard(){
         name='image_upload'
         id='image_upload'
         accept='.jpg, .jpeg, .png'
-        className='form-file' />
+        className='form-file'
+        onChange={imageChange} />
+      {selectedImage && (
+        <div className='form-image-container'>
+          <img
+            src={URL.createObjectURL(selectedImage)}
+            alt="Your image"
+            className='form-image'
+          />
+        </div>
+      )}
       <label>Title</label>
       <input type='text' />
       <label>Text</label>
