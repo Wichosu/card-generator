3a0f715f60660965ccd72625eeaf5570fad19f78
Wicho <luiscalderonmiranda@gmail.com>
2022-06-22 20:58:43 -0500

    modified CardGenerator to CardContainer

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from './Card'
+
+function CardContainer() {
+    return(
+        <div>
+            <Card />
+        </div>
+    );
+}
--- src/components/CardGenerator.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function CardGenerator() {
-    return(
-        <p>hola</p>
-    );
-}
