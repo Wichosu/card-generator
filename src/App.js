eb73bcda9a54576ebe7e357e9d9ecea36db225ed
Wicho <luiscalderonmiranda@gmail.com>
2022-07-25 18:25:21 -0500

    text display none and image blur hover

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import Card from './components/Card';
-import FormCard from './components/FormCard';
 import CardContainer from './components/CardContainer';
 
 function App() {
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,8 @@ function Card({ img, title, description, hasImg}){
   return(
     <div className='card-container'>
       <img src={hasImg? URL.createObjectURL(img) : ''}
-           alt='' 
-           className='card-image'/>
+        alt='' 
+        className='card-image' />
       <h1 className='card-title'>{title}</h1>
       <p className='card-text'>{description}</p>
     </div>
