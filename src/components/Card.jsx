afe4398c9f4509ba5969d4a5953ea284697362ec
Wicho <luiscalderonmiranda@gmail.com>
2022-07-31 19:59:00 -0500

    added icons for edit mode

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../styles/Card.css';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
+import { MdOutlineEdit, MdOutlineEditOff } from 'react-icons/md';
+
 
 function Card({ img, title, description, hasImg, id, deleteCard}){
   return(
     <div className='card-container'>
+      <MdOutlineEdit className='card-edit'></MdOutlineEdit>
+      <MdOutlineEditOff></MdOutlineEditOff>
       <AiOutlineCloseCircle onClick={() => deleteCard(id)} className='card-destroyer'/>
       <img src={hasImg? URL.createObjectURL(img) : ''}
         alt='' 
