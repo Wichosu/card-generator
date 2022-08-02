5c04beffcf5afca8b8af8da65c4fb0d9797f33fe
Wicho <luiscalderonmiranda@gmail.com>
2022-08-01 20:37:43 -0500

    edit title implemented

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,32 +7,48 @@ import { useState } from 'react';
 
 function Card({ img, title, description, hasImg, id, deleteCard}){
 
-  const [edit, setEdit] = useState(true)
-
+  const [edit, setEdit] = useState(false)
+  const [header, setHead] = useState(title)
+  
   const editMode = () => {
     setEdit(!edit)
+  }
+
+  const editHeader = (e) => {
+    setHead(e.target.value)
   }
 
   return(
     <div className='card-container'>
       {
-        edit? <MdOutlineEdit
+        edit? <MdOutlineEditOff
                 onClick={editMode} 
                 className='card-edit' />
               : 
-              <MdOutlineEditOff 
+              <MdOutlineEdit 
                 onClick={editMode}
                 className='card-edit' />
       }
-      <AiOutlineCloseCircle
-        onClick={() => deleteCard(id)} 
-        className='card-destroyer' />
       <img
         src={hasImg? URL.createObjectURL(img) : ''}
         alt='' 
         className='card-image' />
-      <h1 className='card-title'>{title}</h1>
+      {
+        edit? <input 
+                type='text'
+                placeholder={header} 
+                onChange={editHeader}/>
+              :
+              <h1 
+                onClick={edit? editHeader : ''}
+                className='card-title'>{header}
+              </h1>
+      }
+      
       <p className='card-text'>{description}</p>
+      <AiOutlineCloseCircle
+        onClick={() => deleteCard(id)} 
+        className='card-destroyer' />
     </div>
   );
 }
