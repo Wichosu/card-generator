ee22d2bdc053c6d8b6dde4e3ad8ea183b726584e
Wicho <luiscalderonmiranda@gmail.com>
2022-08-19 15:01:55 -0500

    added async and expand cardController.js

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,6 +1,17 @@
+var Cards = require('../models/cards');
 
+var async = require('async');
 
-
-exports.index = (req, res) => {
-  res.render('index');
-}
+exports.index_get = (req, res) => {
+  async.parallel(
+    {
+      cards(callback) {
+        Cards.find(callback);
+      },
+    },
+    (err, results) => {
+      res.render('index', {
+        title: 'Testing',
+      });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const cardController = require('../controllers/cardController');
 
 /*Get Home page */
-router.get('/', cardController.index);
+router.get('/', cardController.index_get);
+//Submit a card
+router.post('/', cardController.index_post);
 
 module.exports = router;
