f40ef0dd8ea2e92e397d10a9712e38a53c4d9be1
Wicho <luiscalderonmiranda@gmail.com>
2022-08-19 14:24:08 -0500

    moved styles, install nodemon, set express

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import CardContainer from './components/CardContainer';
 
 function App() {
--- src/server.js
+++ src/server.js
@@ -16,3 +16,31 @@ mongoose.connect(mongoDB, {
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
+
+
+app.use('/');
+
+//Catch 404 and foward error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+//error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development'? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
